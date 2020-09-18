d9170c567ea86b1d4f8cf6ce8e49f191d731658c
cyyber <kaushal.forex@gmail.com>
2020-09-18 13:45:20 +0530

    Mainchain metadata maintains data related to current mainchain and lastblock

---
+++ metadata/mainchain.go
@@ -0,0 +1,87 @@
+package metadata
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/theQRL/zond/db"
+	"github.com/theQRL/zond/protos"
+	"go.etcd.io/bbolt"
+)
+
+type MainChainMetaData struct {
+	pbData *protos.MainChainMetaData
+}
+
+func (m *MainChainMetaData) FinalizedBlockHeaderHash() []byte {
+	return m.pbData.FinalizedBlockHeaderHash
+}
+
+func (m *MainChainMetaData) FinalizedBlockSlotNumber() uint64 {
+	return m.pbData.FinalizedBlockSlotNumber
+}
+
+func (m *MainChainMetaData) LastBlockHeaderHash() []byte {
+	return m.pbData.LastBlockHeaderHash
+}
+
+func (m *MainChainMetaData) LastBlockSlotNumber() uint64 {
+	return m.pbData.LastBlockSlotNumber
+}
+
+func (m *MainChainMetaData) Serialize() ([]byte, error) {
+	return proto.Marshal(m.pbData)
+}
+
+func (m *MainChainMetaData) DeSerialize(data []byte) error {
+	return proto.Unmarshal(data, m.pbData)
+}
+
+func (m *MainChainMetaData) Commit(b *bbolt.Bucket) error {
+	data, err := m.Serialize()
+	if err != nil {
+		return err
+	}
+	return b.Put(GetMainChainMetaDataKey(), data)
+}
+
+func (m *MainChainMetaData) UpdateFinalizedBlockData(finalizedBlockHeaderHash []byte,
+	finalizedBlockSlotNumber uint64) {
+	m.pbData.FinalizedBlockHeaderHash = finalizedBlockHeaderHash
+	m.pbData.FinalizedBlockSlotNumber = finalizedBlockSlotNumber
+}
+
+func (m *MainChainMetaData) UpdateLastBlockData(lastBlockHeaderHash []byte,
+	lastBlockSlotNumber uint64) {
+	m.pbData.LastBlockHeaderHash = lastBlockHeaderHash
+	m.pbData.LastBlockSlotNumber = lastBlockSlotNumber
+}
+
+func NewMainChainMetaData(finalizedBlockHeaderHash []byte, finalizedBlockSlotNumber uint64,
+	lastBlockHeaderHash []byte, lastBlockSlotNumber uint64) *MainChainMetaData {
+	pbData := &protos.MainChainMetaData {
+		FinalizedBlockHeaderHash: finalizedBlockHeaderHash,
+		FinalizedBlockSlotNumber: finalizedBlockSlotNumber,
+
+		LastBlockHeaderHash: lastBlockHeaderHash,
+		LastBlockSlotNumber: lastBlockSlotNumber,
+	}
+	return &MainChainMetaData {
+		pbData: pbData,
+	}
+}
+
+func GetMainChainMetaData(d *db.DB) (*MainChainMetaData, error) {
+	key := GetMainChainMetaDataKey()
+	data, err := d.Get(key)
+	if err != nil {
+		return nil, err
+	}
+	m := &MainChainMetaData{
+		pbData: &protos.MainChainMetaData{},
+	}
+	return m, m.DeSerialize(data)
+}
+
+func GetMainChainMetaDataKey() []byte {
+	return []byte(fmt.Sprintf("MAIN-CHAIN-META-DATA"))
+}
