e5e237c091cf98d10f2c83ca2d2dcb4a151a92fa
cyyber <kaushal.forex@gmail.com>
2020-11-18 16:53:13 +0530

    Added PeerData and PeerInfo to store connected and disconnected peer info

--- protos/metadata.pb.go
+++ protos/metadata.pb.go
@@ -695,6 +695,124 @@ func (x *BlockHashesBySlotNumber) GetHeaderHashes() [][]byte {
 	return nil
 }
 
+type PeerInfo struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Ip        string `protobuf:"bytes,1,opt,name=ip,proto3" json:"ip,omitempty"`
+	Port      uint32 `protobuf:"varint,2,opt,name=port,proto3" json:"port,omitempty"`
+	Timestamp uint64 `protobuf:"varint,3,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
+}
+
+func (x *PeerInfo) Reset() {
+	*x = PeerInfo{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protos_metadata_proto_msgTypes[10]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PeerInfo) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PeerInfo) ProtoMessage() {}
+
+func (x *PeerInfo) ProtoReflect() protoreflect.Message {
+	mi := &file_protos_metadata_proto_msgTypes[10]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PeerInfo.ProtoReflect.Descriptor instead.
+func (*PeerInfo) Descriptor() ([]byte, []int) {
+	return file_protos_metadata_proto_rawDescGZIP(), []int{10}
+}
+
+func (x *PeerInfo) GetIp() string {
+	if x != nil {
+		return x.Ip
+	}
+	return ""
+}
+
+func (x *PeerInfo) GetPort() uint32 {
+	if x != nil {
+		return x.Port
+	}
+	return 0
+}
+
+func (x *PeerInfo) GetTimestamp() uint64 {
+	if x != nil {
+		return x.Timestamp
+	}
+	return 0
+}
+
+type PeerData struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	ConnectedPeers    []*PeerInfo `protobuf:"bytes,1,rep,name=connected_peers,json=connectedPeers,proto3" json:"connected_peers,omitempty"`
+	DisconnectedPeers []*PeerInfo `protobuf:"bytes,2,rep,name=disconnected_peers,json=disconnectedPeers,proto3" json:"disconnected_peers,omitempty"`
+}
+
+func (x *PeerData) Reset() {
+	*x = PeerData{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protos_metadata_proto_msgTypes[11]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PeerData) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PeerData) ProtoMessage() {}
+
+func (x *PeerData) ProtoReflect() protoreflect.Message {
+	mi := &file_protos_metadata_proto_msgTypes[11]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PeerData.ProtoReflect.Descriptor instead.
+func (*PeerData) Descriptor() ([]byte, []int) {
+	return file_protos_metadata_proto_rawDescGZIP(), []int{11}
+}
+
+func (x *PeerData) GetConnectedPeers() []*PeerInfo {
+	if x != nil {
+		return x.ConnectedPeers
+	}
+	return nil
+}
+
+func (x *PeerData) GetDisconnectedPeers() []*PeerInfo {
+	if x != nil {
+		return x.DisconnectedPeers
+	}
+	return nil
+}
+
 var File_protos_metadata_proto protoreflect.FileDescriptor
 
 var file_protos_metadata_proto_rawDesc = []byte{
@@ -802,10 +920,23 @@ var file_protos_metadata_proto_rawDesc = []byte{
 	0x75, 0x6d, 0x62, 0x65, 0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x04, 0x52, 0x0a, 0x73, 0x6c, 0x6f,
 	0x74, 0x4e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x12, 0x23, 0x0a, 0x0d, 0x68, 0x65, 0x61, 0x64, 0x65,
 	0x72, 0x5f, 0x68, 0x61, 0x73, 0x68, 0x65, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x0c, 0x52, 0x0c,
-	0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x48, 0x61, 0x73, 0x68, 0x65, 0x73, 0x42, 0x1f, 0x5a, 0x1d,
-	0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x74, 0x68, 0x65, 0x51, 0x52,
-	0x4c, 0x2f, 0x7a, 0x6f, 0x6e, 0x64, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x62, 0x06, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x48, 0x61, 0x73, 0x68, 0x65, 0x73, 0x22, 0x4c, 0x0a, 0x08,
+	0x50, 0x65, 0x65, 0x72, 0x49, 0x6e, 0x66, 0x6f, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x70, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x70, 0x12, 0x12, 0x0a, 0x04, 0x70, 0x6f, 0x72, 0x74,
+	0x18, 0x02, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x04, 0x70, 0x6f, 0x72, 0x74, 0x12, 0x1c, 0x0a, 0x09,
+	0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x18, 0x03, 0x20, 0x01, 0x28, 0x04, 0x52,
+	0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x22, 0x86, 0x01, 0x0a, 0x08, 0x50,
+	0x65, 0x65, 0x72, 0x44, 0x61, 0x74, 0x61, 0x12, 0x39, 0x0a, 0x0f, 0x63, 0x6f, 0x6e, 0x6e, 0x65,
+	0x63, 0x74, 0x65, 0x64, 0x5f, 0x70, 0x65, 0x65, 0x72, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b,
+	0x32, 0x10, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2e, 0x50, 0x65, 0x65, 0x72, 0x49, 0x6e,
+	0x66, 0x6f, 0x52, 0x0e, 0x63, 0x6f, 0x6e, 0x6e, 0x65, 0x63, 0x74, 0x65, 0x64, 0x50, 0x65, 0x65,
+	0x72, 0x73, 0x12, 0x3f, 0x0a, 0x12, 0x64, 0x69, 0x73, 0x63, 0x6f, 0x6e, 0x6e, 0x65, 0x63, 0x74,
+	0x65, 0x64, 0x5f, 0x70, 0x65, 0x65, 0x72, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x10,
+	0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2e, 0x50, 0x65, 0x65, 0x72, 0x49, 0x6e, 0x66, 0x6f,
+	0x52, 0x11, 0x64, 0x69, 0x73, 0x63, 0x6f, 0x6e, 0x6e, 0x65, 0x63, 0x74, 0x65, 0x64, 0x50, 0x65,
+	0x65, 0x72, 0x73, 0x42, 0x1f, 0x5a, 0x1d, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f,
+	0x6d, 0x2f, 0x74, 0x68, 0x65, 0x51, 0x52, 0x4c, 0x2f, 0x7a, 0x6f, 0x6e, 0x64, 0x2f, 0x70, 0x72,
+	0x6f, 0x74, 0x6f, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -820,7 +951,7 @@ func file_protos_metadata_proto_rawDescGZIP() []byte {
 	return file_protos_metadata_proto_rawDescData
 }
 
-var file_protos_metadata_proto_msgTypes = make([]protoimpl.MessageInfo, 10)
+var file_protos_metadata_proto_msgTypes = make([]protoimpl.MessageInfo, 12)
 var file_protos_metadata_proto_goTypes = []interface{}{
 	(*DilithiumMetaData)(nil),        // 0: protos.DilithiumMetaData
 	(*SlaveMetaData)(nil),            // 1: protos.SlaveMetaData
@@ -832,16 +963,20 @@ var file_protos_metadata_proto_goTypes = []interface{}{
 	(*MainChainMetaData)(nil),        // 7: protos.MainChainMetaData
 	(*EpochBlockHashesMetaData)(nil), // 8: protos.EpochBlockHashesMetaData
 	(*BlockHashesBySlotNumber)(nil),  // 9: protos.BlockHashesBySlotNumber
+	(*PeerInfo)(nil),                 // 10: protos.PeerInfo
+	(*PeerData)(nil),                 // 11: protos.PeerData
 }
 var file_protos_metadata_proto_depIdxs = []int32{
-	6, // 0: protos.EpochMetaData.slot_info:type_name -> protos.SlotInfo
-	5, // 1: protos.EpochMetaData.prev_epoch_stake_data:type_name -> protos.EpochStakeData
-	9, // 2: protos.EpochBlockHashesMetaData.block_hashes_by_slot_number:type_name -> protos.BlockHashesBySlotNumber
-	3, // [3:3] is the sub-list for method output_type
-	3, // [3:3] is the sub-list for method input_type
-	3, // [3:3] is the sub-list for extension type_name
-	3, // [3:3] is the sub-list for extension extendee
-	0, // [0:3] is the sub-list for field type_name
+	6,  // 0: protos.EpochMetaData.slot_info:type_name -> protos.SlotInfo
+	5,  // 1: protos.EpochMetaData.prev_epoch_stake_data:type_name -> protos.EpochStakeData
+	9,  // 2: protos.EpochBlockHashesMetaData.block_hashes_by_slot_number:type_name -> protos.BlockHashesBySlotNumber
+	10, // 3: protos.PeerData.connected_peers:type_name -> protos.PeerInfo
+	10, // 4: protos.PeerData.disconnected_peers:type_name -> protos.PeerInfo
+	5,  // [5:5] is the sub-list for method output_type
+	5,  // [5:5] is the sub-list for method input_type
+	5,  // [5:5] is the sub-list for extension type_name
+	5,  // [5:5] is the sub-list for extension extendee
+	0,  // [0:5] is the sub-list for field type_name
 }
 
 func init() { file_protos_metadata_proto_init() }
@@ -970,6 +1105,30 @@ func file_protos_metadata_proto_init() {
 				return nil
 			}
 		}
+		file_protos_metadata_proto_msgTypes[10].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PeerInfo); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_protos_metadata_proto_msgTypes[11].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PeerData); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -977,7 +1136,7 @@ func file_protos_metadata_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_protos_metadata_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   10,
+			NumMessages:   12,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
